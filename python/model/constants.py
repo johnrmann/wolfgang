ebf6460f168721341c7b4952f2d72c6ef8f6e69c
johnrmann <accounts@jmann.io>
2025-04-06 10:41:15 -0700

    Merge jm/cuda into master: add support for cuda training.

--- python/model/constants.py
+++ python/model/constants.py
@@ -1,8 +1,13 @@
 import torch
 
-DEVICE = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
-SEQ_LENGTH = 32
-BATCH_SIZE = 16
+DEVICE = torch.device('cpu')
+if torch.cuda.is_available():
+	DEVICE = torch.device('cuda')
+elif torch.backends.mps.is_available():
+	DEVICE = torch.device('mps')
+
+SEQ_LENGTH = 64
+BATCH_SIZE = 8
 EMBED_SIZE = 256
 N_HEADS = 8
 TRANSFORMER_LAYERS = 6
